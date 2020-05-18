e967631ef89e324d3bd339f334b1d1eb386a01e4
piotrszleg <szlegp@gmail.com>
2020-05-19 00:22:44 +0200

    Testy i implementacja kopca.

---
+++ src/BinomialHeap.java
@@ -0,0 +1,225 @@
+public class BinomialHeap<T extends Comparable<T>> {
+    public static class Node<T> {
+        T key;
+        Node<T> p;
+        Node<T> sibling;
+        Node<T> child;
+        int degree;
+    }
+    Node<T> head;
+
+    public BinomialHeap(){}
+
+    private boolean isCorrectRecursive(Node<T> node){
+        if(node==null) {
+            return true;
+        } if(node.p!=null && node.p.key.compareTo(node.key)>0){
+            // key in parent needs to be smaller than key in children
+            return false;
+        } else {
+            return isCorrectRecursive(node.sibling) && isCorrectRecursive(node.child);
+        }
+    }
+
+    // used for testing
+    boolean isCorrect(){
+        return isCorrectRecursive(head);
+    }
+
+    public BinomialHeap(T[] array){
+        for(T key : array){
+            insert(key);
+        }
+    }
+
+    public Node<T> insert(T key){
+        BinomialHeap<T> newH=new BinomialHeap<T>();
+        Node<T> x= new Node<>();
+        x.p=null;
+        x.child=null;
+        x.sibling=null;
+        x.degree=0;
+        x.key=key;
+        newH.head=x;
+        head=union(this, newH).head;
+        return x;
+    }
+
+    Node<T> findRecursive(Node<T> node, T key){
+        if(node==null){
+            return null;
+        } else if(node.key==key){
+            return node;
+        } else {
+            Node<T> fromChild=findRecursive(node.child, key);
+            if(fromChild!=null) {
+                return fromChild;
+            } else {
+                return findRecursive(node.sibling, key);
+            }
+        }
+    }
+
+    Node<T> find(T key) {
+        return findRecursive(head, key);
+    }
+
+    boolean contains(T key){
+        return find(key)!=null;
+    }
+
+    T minimum(){
+        Node<T> y=null;
+        Node<T> x=head;
+        T min=null;
+        while(x!=null){
+            if(min==null || x.key.compareTo(min)<0){
+                min=x.key;
+                y=x;
+            }
+            x=x.sibling;
+        }
+        return y.key;
+    }
+
+    private Node<T> extractMinRoot(){
+        // Znajdź korzeń x z minimalnym kluczem na liście korzeni H
+        // i usuń x z listy korzeni H
+        Node<T> x = null;
+        Node<T> previous = null;
+        Node<T> x_previous = null;
+        for (Node<T> current = head; current != null; previous = current, current = current.sibling) {
+            if (x == null || current.key.compareTo(x.key) < 0) {
+                x = current;
+                x_previous = previous;
+            }
+        }
+        if (x_previous != null) {
+            x_previous.sibling = x.sibling;
+        } else {
+            // x was head
+            head = x.sibling;
+        }
+        return x;
+    }
+
+    private Node<T> reverseChildren(Node<T> x){
+        // Odwróć kolejność elementów na liście synów węzła x i zapamiętaj
+        // wskaźnik do głowy wynikowej listy w zmiennej head[H‘]
+        Node<T> previous = null;
+        Node<T> current = x.child;
+        while(current!=null) {
+            Node<T> next = current.sibling;
+            current.sibling = previous;
+            previous=current;
+            current=next;
+        }
+        return previous;
+    }
+
+    public Node<T> extractMin(){
+        Node<T> x = extractMinRoot();
+        BinomialHeap<T> newH=new BinomialHeap<>();
+        newH.head=reverseChildren(x);
+        head=union(this,newH).head;
+        return x;
+    }
+
+    static <T> void BinomialLink(Node<T> y, Node<T> parent) {
+        y.p = parent;
+        y.sibling=parent.child;
+        parent.child=y;
+        parent.degree=parent.degree + 1;
+    }
+
+    private static <T extends Comparable<T>> Node<T> BinomialHeapMerge(BinomialHeap<T> H1, BinomialHeap<T> H2){
+        Node<T> left=H1.head;
+        Node<T> right=H2.head;
+
+        Node<T> result=null;
+        Node<T> current=null;
+        while(left!=null || right!=null) {
+            Node<T> toInsert;
+            if (right == null) {
+                toInsert = left;
+                left=left.sibling;
+            } else if (left == null) {
+                toInsert = right;
+                right=right.sibling;
+            } else if (left.degree <= right.degree) {
+                toInsert = left;
+                left=left.sibling;
+            } else {
+                toInsert = right;
+                right=right.sibling;
+            }
+            if(result==null){
+                result=toInsert;
+                current=toInsert;
+            } else {
+                current.sibling=toInsert;
+                current=toInsert;
+            }
+        }
+        return result;
+    }
+
+    // H1 and H2 are destroyed in process
+    public static <T extends Comparable<T>> BinomialHeap<T> union(BinomialHeap<T> H1, BinomialHeap<T> H2){
+        BinomialHeap<T> H = new BinomialHeap<>();
+        H.head=BinomialHeapMerge(H1, H2);
+        // Zwolnij obiekty H1 and H2, ale nie listy, na które wskazują
+        if (H.head==null) {
+            return H;
+        }
+        Node<T> prev_x=null;
+        Node<T> x=H.head;
+        Node<T> next_x=x.sibling;
+        while (next_x != null) {
+            if ((x.degree != next_x.degree) || (next_x.sibling != null &&
+                    next_x.sibling.degree == x.degree)) {
+                prev_x = x; // przypadek 1 i 2
+                x = next_x; // przypadek 1 i 2
+            } else {
+                if (x.key.compareTo(next_x.key)<=0) {
+                    x.sibling = next_x.sibling; // przypadek 3
+                    BinomialLink(next_x, x); // przypadek 3
+                } else {
+                    if (prev_x == null) { // przypadek 4
+                        H.head = next_x; // przypadek 4
+                    } else { // przypadek 4
+                        prev_x.sibling = next_x; // przypadek 4
+                    }
+                    BinomialLink(x, next_x); // przypadek 4
+                    x = next_x; // przypadek 4
+                }
+            }
+            next_x = x.sibling;
+        }
+        // H1 and H2 are destroyed in process
+        H1.head=null;
+        H2.head=null;
+        return H;
+    }
+    void decreaseKey(Node<T> x, T k){
+        if(k.compareTo(x.key)>0) {
+            throw new IllegalArgumentException("new key is greater than current key");
+        }
+        x.key=k;
+        Node<T> y=x;
+        Node<T> z=y.p;
+        while(z!=null && y.key.compareTo(z.key)<0) {// key[y]<key[z]
+            // exchange key[y]and key[z]
+            T temp=y.key;
+            y.key=z.key;
+            z.key=temp;
+            // if y and z have satellite fields, exchange them, too
+            y=z;
+            z=y.p;
+        }
+    }
+    void delete(Node<T> x, T leastT){
+        decreaseKey(x, leastT);
+        extractMin();
+    }
+}
---
+++ src/BinomialHeapTest.java
@@ -0,0 +1,90 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinomialHeapTest {
+
+    Integer[] testArray=new Integer[]{1,5,6};
+
+    @org.junit.jupiter.api.Test
+    void insertContains() {
+        BinomialHeap<Integer> heap=new BinomialHeap<>();
+        for(Integer i : testArray) {
+            heap.insert(i);
+        }
+        assertTrue(testHeap.isCorrect());
+        for(Integer i : testArray) {
+            assertTrue(heap.contains(i));
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void arrayConstructor() {
+        BinomialHeap<Integer> heap=new BinomialHeap<>(testArray);
+        assertTrue(testHeap.isCorrect());
+        for(Integer i : testArray) {
+            assertTrue(heap.contains(i));
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void union() {
+        Integer[] array1=new Integer[]{1,5,6};
+        Integer[] array2=new Integer[]{4,8,6};
+        BinomialHeap<Integer> heap1=new BinomialHeap<>(array1);
+        assertTrue(heap1.isCorrect());
+        BinomialHeap<Integer> heap2=new BinomialHeap<>(array2);
+        assertTrue(heap2.isCorrect());
+        BinomialHeap<Integer> union=BinomialHeap.union(heap1, heap2);
+        // union arguments need to be cleared
+        assertNull(heap1.head);
+        assertNull(heap2.head);
+        for(Integer i : array1){
+            assertTrue(union.contains(i));
+        }
+        for(Integer i : array2){
+            assertTrue(union.contains(i));
+        }
+    }
+
+    BinomialHeap<Integer> testHeap;
+
+    @BeforeEach
+    void setUp() {
+        testHeap=new BinomialHeap<>(new Integer[]{5, 8, 10, 12, 1, 5, 20, 6, 7, 17});
+    }
+
+    @org.junit.jupiter.api.Test
+    void minimum() {
+        assertEquals(testHeap.minimum(), 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void extractMin() {
+        assertEquals(testHeap.extractMin().key, 1);
+        assertTrue(testHeap.isCorrect());
+        assertFalse(testHeap.contains(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void decreaseKey() {
+        BinomialHeap.Node<Integer> node=testHeap.insert(7);
+        testHeap.decreaseKey(node, 3);
+        assertTrue(testHeap.isCorrect());
+    }
+
+    @org.junit.jupiter.api.Test
+    void decreaseKeyException() {
+        BinomialHeap.Node<Integer> node=testHeap.insert(7);
+        assertThrows(IllegalArgumentException.class,
+                ()->testHeap.decreaseKey(node, 10));
+        assertTrue(testHeap.isCorrect());
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+        BinomialHeap.Node<Integer> node=testHeap.insert(7);
+        testHeap.delete(node, Integer.MIN_VALUE);
+        assertTrue(testHeap.contains(7));
+    }
+}
